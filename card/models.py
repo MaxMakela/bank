708b9b0f4387646df80b80dc0489fb42fdc2049d
MaxMakela <johnhopeful11@gmail.com>
2019-08-16 22:18:54 +0300

    Session logic changed to database logic

--- card/models.py
+++ card/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime, timezone, timedelta
+from django.utils import timezone
 from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import (
@@ -8,41 +9,67 @@ from django.contrib.auth.models import (
 )
 
 USERNAME_REGEX = '^[0-9]*$'
+PIN_REGEX = '^[0-9]*$'
+
 
 class AccManager(BaseUserManager):
-  def create_user(self, cardID, password=None):
-    if not cardID:
-      raise ValueError('Users must have a username')
-    user = self.model(cardID = cardID)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+    def create_user(self, card_id, password=None):
+        if not card_id:
+            raise ValueError('Users must have a username')
+        user = self.model(card_id=card_id)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-
-  def create_superuser(self, cardID, password=None):
-    user = self.create_user(cardID, password=password)
-    user.is_staff = True
-    user.is_admin = True
-    user.is_superuser = True
-    user.save(using=self._db)
-    return user
-
-
+    def create_superuser(self, card_id, password=None):
+        user = self.create_user(card_id, password=password)
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class CardInfo(AbstractBaseUser, PermissionsMixin):
-  cardID = models.CharField(
+    card_id = models.CharField(
         max_length=16,
-        validators=[RegexValidator(regex=USERNAME_REGEX)], 
+        validators=[RegexValidator(regex=USERNAME_REGEX)],
         unique=True)
-  balance = models.BigIntegerField(default=0)
-  is_active = models.BooleanField(default=True)
-  valid_thru = models.DateField(default=datetime.now() + timedelta(days=365))
-  is_superuser = models.BooleanField(default=False)
-  is_admin = models.BooleanField(default=False)
-  is_staff = models.BooleanField(default=False)
+    balance = models.BigIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    valid_thru = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=365))
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-  objects = AccManager()
-    
-  USERNAME_FIELD = 'cardID'
-  REQUIRED_FIELDS = []
+    objects = AccManager()
+
+    USERNAME_FIELD = 'card_id'
+    REQUIRED_FIELDS = []
+
+
+class LoginInfo(models.Model):
+    card_id = models.CharField(
+        max_length=16,
+        validators=[RegexValidator(regex=USERNAME_REGEX)])
+    time = models.DateTimeField(default=timezone.now)
+
+
+class Transaction(models.Model):
+    card_id = models.CharField(
+        max_length=16,
+        validators=[RegexValidator(regex=USERNAME_REGEX)])
+    operation = models.BooleanField(default=True)
+    value = models.BigIntegerField(default=0)
+    time = models.DateTimeField(default=timezone.now)
+    new_balance = models.BigIntegerField(default=0)
+
+
+class PinSaver(models.Model):
+    card_id = models.CharField(
+        max_length=16,
+        validators=[RegexValidator(regex=USERNAME_REGEX)])
+    new_pin = models.CharField(
+        max_length=4,
+        validators=[RegexValidator(regex=PIN_REGEX)])
+    confirm = models.BooleanField(default=False)
--- card/views.py
+++ card/views.py
@@ -4,141 +4,172 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import AbstractBaseUser
+from card.models import LoginInfo, Transaction, PinSaver
+from datetime import datetime, timezone, timedelta
+from django.db.models import F
+from django.utils.timezone import get_current_timezone
 
+User = get_user_model()
 
 # тут все на столько сырое, что скоро выростут грибы
 
 
 @login_required(redirect_field_name='login_card')
 def home(request):
-	context = {}
-	if request.session.get('new_pin'):
-		del request.session['new_pin']
-	return render(request, 'card/home.html', context)
+    context = {}
+    if request.session.get('new_pin'):
+        del request.session['new_pin']
+    return render(request, 'card/home.html', context)
 
 
 def login_card(request):
-	context = {}
-	if request.method == "POST":
-		cardID = request.POST['username']
-		password = request.POST['password']
-		if cardID and password:
-			User = get_user_model()
-			card = authenticate(request, cardID=cardID, password=password)
-			if card:
-				login(request, card)
-				return HttpResponseRedirect(reverse('home'))
-			else:
-				if User.objects.filter(cardID=cardID).exists():
-					context['error'] = 'Wrong pin!'
-					if cardID in request.session:
-						request.session[cardID] += 1
-					else:
-						request.session[cardID] = 1
-					request.session.modified = True
-					if request.session.get(cardID) >= 3:
-						card = User.objects.get(cardID=cardID)
-						card.is_active = False
-						card.save()
-						context['error'] = 'Card has been blocked! To unlock, contact the bank employees.'
-					return render(request, 'card/login.html', context)
-				context['error'] = 'Wrong card ID!'
-				return render(request, 'card/login.html', context)
-		else:
-			context['error'] = 'You need enter the card number and pin!'
-			return render(request, 'card/login.html', context)
-	else:
-		return render(request, 'card/login.html', context)
+    context = {}
+    if request.method == "POST":
+        card_id = request.POST['username']
+        password = request.POST['password']
+        if card_id and password:
+            card = authenticate(request, card_id=card_id, password=password)
+            if card:
+                login(request, card)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                context['error'] = 'Wrong pin or card ID!'
+                if User.objects.filter(card_id=card_id).exists():
+                    incorect_pin_try = LoginInfo(card_id=card_id)
+                    incorect_pin_try.save()
+                    if len(LoginInfo.objects.filter(card_id=card_id, time__gt=datetime.now(tz=get_current_timezone())-timedelta(minutes=3))) >= 3:
+                        card = User.objects.get(card_id=card_id)
+                        card.is_active = False
+                        card.save()
+                        context['error'] = 'Card has been blocked! To unlock, contact the bank employees.'
+                    return render(request, 'card/login.html', context)
+                return render(request, 'card/login.html', context)
+        else:
+            context['error'] = 'You need enter the card number and pin!'
+            return render(request, 'card/login.html', context)
+    else:
+        return render(request, 'card/login.html', context)
 
 
 @login_required(redirect_field_name='login_card')
 def balance(request):
-	return render(request, 'card/balance.html')
+    return render(request, 'card/balance.html')
 
 
 @login_required(redirect_field_name='login_card')
 def refill(request):
-	context = {}
-	if request.method == "POST":
-		if request.POST['summ']:
-			summ = int(request.POST['summ'])
-		else:
-			context['comment'] = 'Enter the amount you want to add to your account'
-			return render(request, 'card/cash.html', context)
-		card = request.user
-		card.balance = card.balance+summ
-		card.save()
-		context['success'] = 'Operation done successful'
-		return render(request, 'card/refill.html', context)
-	else:
-		return render(request, 'card/refill.html', context)
+    context = {}
+    if request.method == "POST":
+        if request.POST['summ']:
+            summ = int(request.POST['summ'])
+            if summ < 0:
+                summ *= -1
+        else:
+            context['comment'] = 'Enter the amount you want to add to your account'
+            return render(request, 'card/cash.html', context)
+        card = request.user
+        card.balance = card.balance+summ
+        transact = Transaction(card_id=card, operation=True, value=summ, new_balance=card.balance)
+        if card.balance >= 0:
+            transact.save()
+            card.save()
+        context['success'] = 'Operation done successful'
+        return render(request, 'card/refill.html', context)
+    else:
+        return render(request, 'card/refill.html', context)
 
 
 @login_required(redirect_field_name='login_card')
 def cash(request):
-	context = {}
-	if request.method == "POST":
-		if request.POST['summ']:
-			summ = int(request.POST['summ'])
-		else:
-			context['comment'] = 'Enter the amount you want to withdraw'
-			return render(request, 'card/cash.html', context)
-		card = request.user
-		if card.balance >= summ:
-			card.balance = card.balance-summ
-			card.save()
-			context['comment'] = 'Operation done successful'
-			return render(request, 'card/cash.html', context)
-		else:
-			context['comment'] = 'Insufficient funds in your account!'
-			return render(request, 'card/cash.html', context)
+    context = {}
+    if request.method == "POST":
+        if request.POST['summ']:
+            summ = int(request.POST['summ'])
+            if summ < 0:
+                summ *= -1
+        else:
+            context['comment'] = 'Enter the amount you want to withdraw'
+            return render(request, 'card/cash.html', context)
+        card = request.user
+        if card.balance >= summ:
+            card.balance = card.balance-summ
+            transact = Transaction(card_id=card, operation=False, value=summ, new_balance=card.balance)
+            if card.balance >= 0:
+                transact.save()
+                card.save()
+            context['comment'] = 'Operation done successful'
+            return render(request, 'card/cash.html', context)
+        else:
+            context['comment'] = 'Insufficient funds in your account!'
+            return render(request, 'card/cash.html', context)
+    else:
+        return render(request, 'card/cash.html', context)
 
-	else:
-		return render(request, 'card/cash.html', context)
 
-
+# Мне стыдно за этот метод. Простите, что читаете что это
 @login_required(redirect_field_name='login_card')
 def pin_change(request):
-	context = {}
-	if request.method == "POST":
-		
-		card = request.user
-		new_pin = request.POST['password']
-		
-		if request.session.get('new_pin'):
-			if request.session.get('new_pin') == new_pin:
-				card.set_password(new_pin)
-				card.save()
-				del request.session['new_pin']
-				context['comment'] = 'Your pin was changed'
-				return render(request, 'card/pin_change.html', context)
-			else:
-				del request.session['new_pin']
-				context['comment'] = 'Pins do not match. Try again'
-				return render(request, 'card/pin_change.html', context)
-		
-		if not card.check_password(new_pin):
-			if pin_valid(new_pin):
-				request.session['new_pin'] = new_pin
-				context['comment'] = 'Confirm pin'
-				return render(request, 'card/pin_change.html', context)
-			else:
-				context['comment'] = 'Incorrect pin format. Try another one'
-				return render(request, 'card/pin_change.html', context)
-		else:
-			context['comment'] = 'New pin can not be the same old'
-			return render(request, 'card/pin_change.html', context)
-	else:
-		return render(request, 'card/pin_change.html', context)
+    context = {}
+    card = request.user
+
+    if request.method == "POST":
+
+        new_pin = request.POST['password']
+
+        if PinSaver.objects.filter(card_id=card):
+            pin_obj_list = PinSaver.objects.filter(card_id=card)
+            pin_obj = PinSaver.objects.filter(card_id=card)[len(pin_obj_list)-1]
+            if not pin_obj.confirm:
+                if pin_obj.new_pin == new_pin:
+                    card.set_password(new_pin)
+                    card.save()
+                    pin_obj.confirm = True
+                    pin_obj.save()
+                    context['comment'] = 'Your pin was changed'
+                    return render(request, 'card/pin_change.html', context)
+                else:
+                    pin_obj.confirm = True
+                    pin_obj.save()
+                    context['comment'] = 'Pins do not match. Try again'
+                    return render(request, 'card/pin_change.html', context)
+            else:
+                return render(request, 'card/pin_change.html', current_pin_valid_context(card, new_pin))
+        else:
+            return render(request, 'card/pin_change.html', current_pin_valid_context(card, new_pin))
+        
+    else:
+        if PinSaver.objects.filter(card_id=card):
+            pin_obj_list = PinSaver.objects.filter(card_id=card)
+            pin_obj = PinSaver.objects.filter(card_id=card)[len(pin_obj_list) - 1]
+            if not pin_obj.confirm:
+                    pin_obj.confirm = True
+                    pin_obj.save()
+        return render(request, 'card/pin_change.html', context)
 
 
 @login_required(redirect_field_name='login_card')
 def logout_card(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('login_card'))
+    logout(request)
+    return HttpResponseRedirect(reverse('login_card'))
 
 
 def pin_valid(pin):
-	return False if len(pin) != 4 or pin[0] == pin[1] or pin[2] == pin[3] else True
+    try:
+        return False if len(pin) != 4 or int(pin[0]) == int(pin[1]) or int(pin[2]) == int(pin[3]) else True
+    except ValueError:
+        return False
 
 
+def current_pin_valid_context(card, new_pin):
+    context = {}
+    if not card.check_password(new_pin):
+        if pin_valid(new_pin):
+            PinSaver(card_id=card, new_pin=new_pin).save()
+            context['comment'] = 'Confirm pin'
+            return context
+        else:
+            context['comment'] = 'Incorrect pin format. Try another one'
+            return context
+    else:
+        context['comment'] = 'New pin can not be the same old'
+        return context
